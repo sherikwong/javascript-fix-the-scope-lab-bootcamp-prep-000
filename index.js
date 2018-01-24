f9667aec9b6d84afd99473289872e76d578eee7b
Sheri Kwong <sherikwo@buffalo.edu>
2018-01-24 19:36:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -30,25 +30,3 @@ var theFunk = funkyFunction
 
 
 
-
-  describe('add2(n)', function() {
-    it('adds two to n', function() {
-      const n = Math.floor(Math.random() * 1000)
-      expect(window.add2(n)).toEqual(n + 2)
-    })
-  })
-
-  describe('funkyFunction()', function() {
-    it('returns a function', function() {
-      expect(typeof window.funkyFunction()).toEqual('function')
-    })
-  })
-
-  describe('theFunk', function() {
-    it('is "FUNKY!"', function() {
-      expect(window.theFunk).toEqual('FUNKY!')
-    })
-  })
-
-})
-
